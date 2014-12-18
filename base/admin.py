845808305f10c9032a440381da024bff164546bb
victorarcasrios <victorarcasrios@gmail.com>
2014-12-18 14:41:35 +0100

    Removed Person, added Beneficiary, Worker, Product and ProductCategory. Admin management include

--- base/admin.py
+++ base/admin.py
@@ -1,12 +1,32 @@
 from django.contrib import admin
-from base.models import Person
+from base.models import Beneficiary, Worker, Product, ProductCategory
 
-class PersonAdmin(admin.ModelAdmin):
+
+class BeneficiaryAdmin(admin.ModelAdmin):
 	fieldsets = [
-		("Campos obligatorios", {'fields': ['name', 'surname', 'dni', 'category']}),
-		("Campos opcionales", {'fields': ['phone_number', 'email'], 'classes': ['collapse']})
+		("Campos obligatorios", {'fields': ('name', 'surname', 'dni')}),
+		("Campos opcionales", {'fields': ('phone_number', 'email')})
+	]
+	search_fields = ('name', 'surname', 'dni', 'phone_number', 'email')
+	list_display = ('name', 'surname', 'is_contactable')
+
+class WorkerAdmin(admin.ModelAdmin):
+	list_filter = ['category']
+	search_fields = ('name', 'surname', 'dni', 'phone_number', 'email')
+	list_display = ('name', 'surname', 'category')
+
+## TODO 
+# Show image preview in form
+class ProductAdmin(admin.ModelAdmin):
+	fieldsets = [
+		("Campos obligatorios", {'fields': ('name', 'category')}),
+		("Campos opcionales", {'fields': ('description', 'image')})
 	]
 	list_filter = ['category']
-	search_fields = ['name', 'surname', 'dni', 'phone_number', 'email']
+	search_fields = ['name']
+	list_display = ('name', 'category')
 
-admin.site.register(Person, PersonAdmin)
+admin.site.register(Beneficiary, BeneficiaryAdmin)
+admin.site.register(Worker, WorkerAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(ProductCategory)
--- base/models.py
+++ base/models.py
@@ -1,18 +1,53 @@
 from django.db import models
 from django.core.validators import validate_email, MinLengthValidator
+from django.conf import settings
 
-class Person(models.Model):
+class Beneficiary(models.Model):
 
 	name = models.CharField(max_length = 30)
 	surname = models.CharField(max_length = 40)
 	dni = models.CharField(max_length = 9, unique = True)
 	phone_number = models.IntegerField(null = True, blank = True)
 	email = models.EmailField(unique = True, blank = True, validators=[validate_email])
-	category = models.CharField(max_length = 11, choices = (
-			('B', 'Beneficiary'),
+	
+	def __str__(self):
+		return "{0} {1}".format(self.name, self.surname)
+
+	def is_contactable(self):		
+		return self.phone_number != None or self.email != ''
+
+	is_contactable.admin_order_field = 'is_contactable'
+	is_contactable.boolean = True
+	is_contactable.short_description = 'Contactable'
+
+class Worker(models.Model):
+
+	name = models.CharField(max_length = 30)
+	surname = models.CharField(max_length = 40)
+	dni = models.CharField(max_length = 9, unique = True)
+	phone_number = models.IntegerField()
+	email = models.EmailField(validators=[validate_email])
+	category = models.CharField(max_length = 9, choices = (
 			('E', 'Employee'),
 			('V', 'Volunteer')
 		))
 
 	def __str__(self):
 		return "[{0}] {1} {2}".format(self.category, self.name, self.surname)
+
+class Product(models.Model):
+
+	name = models.CharField(max_length = 30)
+	description = models.TextField(blank = True)
+	category = models.ForeignKey('ProductCategory')
+	image = models.ImageField(upload_to = settings.IMAGES_ABS_URL, blank = True)
+
+	def __str__(self):
+		return "[{0}] {1}".format(self.category, self.name)
+
+class ProductCategory(models.Model):
+
+	name = models.CharField(max_length = 10, primary_key = True)
+
+	def __str__(self):
+		return self.name
