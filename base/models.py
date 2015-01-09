2985a8b03fc3f9824e51223cc7858c33784409f4
victorarcasrios <victorarcasrios@gmail.com>
2015-01-09 14:03:50 +0100

    Added some models for donations managing

--- base/models.py
+++ base/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.core.validators import validate_email, MinLengthValidator
+from django.core.validators import validate_email, MinLengthValidator, MinValueValidator, MaxValueValidator
 from django.conf import settings
 
 class Beneficiary(models.Model):
@@ -35,6 +35,24 @@ class Worker(models.Model):
 	def __str__(self):
 		return "[{0}] {1} {2}".format(self.category, self.name, self.surname)
 
+class Donor(models.Model):
+
+	name = models.CharField(max_length = 30)
+	surname = models.CharField(max_length = 40)
+	dni = models.CharField(max_length = 9, unique = True)
+	phone_number = models.IntegerField()
+	email = models.EmailField(validators=[validate_email])	
+	
+	def __str__(self):
+		return "{0} {1}".format(self.name, self.surname)
+
+	def is_contactable(self):		
+		return self.phone_number != None or self.email != ''
+
+	is_contactable.admin_order_field = 'is_contactable'
+	is_contactable.boolean = True
+	is_contactable.short_description = 'Contactable'
+
 class Product(models.Model):
 
 	name = models.CharField(max_length = 30)
@@ -51,3 +69,27 @@ class ProductCategory(models.Model):
 
 	def __str__(self):
 		return self.name
+
+class Donation(models.Model):
+
+	date = models.DateTimeField(auto_now_add = True)
+	donor = models.ForeignKey('Donor')
+	money = models.DecimalField(max_digits = 7, decimal_places = 2)
+	coin = models.ForeignKey('Coin')
+	
+	def __str__(self):
+		return "[{0}] {1}".format(self.date, self.money)
+
+class PeriodicalDonation(models.Model):
+
+	initial_date = models.DateTimeField(auto_now_add = True)
+	periodicity = models.PositiveIntegerField(null = True, validators = [
+		MinValueValidator(1), MaxValueValidator(355)])
+	money = models.DecimalField(max_digits = 7, decimal_places = 2)
+	coin = models.ForeignKey('Coin')
+
+class Coin(models.Model):
+
+	acronym = models.CharField(max_length = 3, primary_key = True)
+	name = models.CharField(max_length = 30)
+	symbol = models.CharField(max_length = 1)
