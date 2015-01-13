ff09669d31057d273dcaa62140d325890bb92400
victorarcasrios <victorarcasrios@gmail.com>
2015-01-13 23:50:32 +0100

    Some tries with urls+views

--- base/urls.py
+++ base/urls.py
@@ -1,1 +1,9 @@
-urls.py
+from django.conf.urls import patterns, url
+from base import views
+
+urlpatterns = patterns('',
+	url(r'^$', views.index, name = 'index'),
+	url(r'^objects/$', views.objects_list),
+	url(r'^objects/categories/(?P<category_id>\d+)/$', views.objects_list),
+	url(r'^objects/(?P<object_id>\d+)/$', views.object_detail)
+)
--- base/views.py
+++ base/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from models import Product
 
 def index(request):
 	return HttpResponse("Welcome VNGO")
+
+def objects_list(request, category_id = None):
+	if category_id == None:
+		objects = Product.objects.all()
+	else:
+		## ERROR
+		objects = Product.objects.filter(category__id = category_id)
+
+	output = "<p>Categoria {0}</p><ul>".format(category_id)
+	for obj in objects:
+		output += "<li>{0}</li>".format(obj.name)
+	return HttpResponse(output + "</ul>")
+
+def object_detail(request, object_id):
+	return HttpResponse("Object detail #{0}".format(object_id))
